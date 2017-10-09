456cc2f4d0c4e498c0042c320e628de4bfc587c2
Zannain Shafi <zannainshafi@gmail.com>
2017-10-09 11:47:02 -0700

    added rvm get

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,3 +2,4 @@ class StaticController < ApplicationController
   def index
   end
 end
+
--- spec/factories/users.rb
+++
@@ -1,9 +0,0 @@
-FactoryGirl.define do
-  factory :user do
-    name Faker::Name.name
-    email Faker::Internet.email
-    phone_number Faker::PhoneNumber.cell_phone
-    password'password'
-    password_confirmation 'password'
-  end
-end
